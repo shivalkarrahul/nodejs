8e391853f6fa1f689f77fdb881401418ca015db8
Alexandru Calcatinge <alexandru.calcatinge@gmail.com>
2021-11-23 18:14:48 +0000

    updated the server.js file message

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello, Welcome to DevOps CI CD");
+    response.end("Hello, Welcome to the Demo App for Capstone Project 3 - Post Graduate Program - DevOps");
 
 });
 
