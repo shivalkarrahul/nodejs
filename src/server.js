7ba94da83c7c2eeff19e302020de98cc199c686f
santhosh-scalex <107671221+santhosh-scalex@users.noreply.github.com>
2022-12-23 13:12:52 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = 3000; tjjjk
+var port = 3000; tjjj
 server.listen(port);
 module.exports = server
 console.log("Server is available on Port:", port);
