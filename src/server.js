ad6f4037759dfbd28bee6769d30d671823e93795
santhosh-scalex <107671221+santhosh-scalex@users.noreply.github.com>
2023-05-31 13:39:27 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello, Welcome to DevOps CI CD using aws code deploy");
+    response.end("Hello, Welcome to DevOps CI CD using aws code pipeline");
 
 });
 
